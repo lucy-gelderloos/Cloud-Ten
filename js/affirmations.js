62ce596c6563bc46f345a23a803b39ca282b7e41
Erin Ranta <erinran99@gmail.com>
2022-06-30 01:14:58 -0400

    added affirmations

--- js/affirmations.js
+++ js/affirmations.js
@@ -1,8 +1,9 @@
 //enter affirmations into array
-let affirm = ['Placeholder Text1', 'Placeholder Text2'];
+let affirm = ['I am Worthy of Success', 'I am Strong', 'I can accomplish anything I put my Heart and Mind into', 'I can do Hard Things', 'I have Overcome hard things', 'I am Safe', 'I am Enough', 'I Deserve Success'];
 function generateNumberBetween(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 
 let random = generateNumberBetween(0, affirm.length);
 let newEl = document.createElement('p');
